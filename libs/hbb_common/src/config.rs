53c10eb5c6f56be8385a6f64bc32d0a0ceb6d163
xinyu23cs <67848516+xinyu23cs@users.noreply.github.com>
2024-10-27 18:55:28 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["mindray.pw"];
+pub const PUBLIC_RS_PUB_KEY: &str = "QIIbCbM1sk1vmMgeEhAwvS0LNCxwu5PnbW8TkegjtXk=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
